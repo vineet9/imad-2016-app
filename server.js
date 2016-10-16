0e7d0358499defd68ff1898cf21be6697815e265
Vineet Chauhan <vineet.chd09@gmail.com>
2016-10-16 09:42:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,49 +5,118 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var pageone={
-    title:'Page One - Vineet Chauhan',
-    heading:'Page One',
-    date:'Sep 5, 2016',
-    content:`<p>
-                Content of first web Page. Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.
-            </p>
-            <p>
-                Content of first web Page. Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.
-            </p>
-            <p>
-                Content of first web Page. Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.
-            </p>
-            <p>
-                Content of first web Page. Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.Content of first web Page.
-            </p>`
+
+var articles = {
+    'article-one': {
+      title: 'Article One | Tanmai Gopal',
+      heading: 'Article One',
+      date: 'Sep 5, 2016',
+      content: `
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>
+          <p>
+              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+          </p>`
+    },
+    'article-two': {
+      title: 'Article Two | Tanmai Gopal',
+      heading: 'Article Two',
+      date: 'Sep 10, 2016',
+      content: `
+          <p>
+              This is the content for my second article.
+          </p>`
+    },
+    'article-three': {
+      title: 'Article Three | Tanmai Gopal',
+      heading: 'Article Three',
+      date: 'Sep 15, 2016',
+      content: `
+          <p>
+              This is the content for my third article.
+          </p>`
+    }
 };
+
+function createTemplate (data) {
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+    var htmlTemplate = `
+    <html>
+      <head>
+          <title>
+              ${title}
+          </title>
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link href="/ui/style.css" rel="stylesheet" />
+      </head> 
+      <body>
+          <div class="container">
+              <div>
+                  <a href="/">Home</a>
+              </div>
+              <hr/>
+              <h3>
+                  ${heading}
+              </h3>
+              <div>
+                  ${date}
+              </div>
+              <div>
+                ${content}
+              </div>
+          </div>
+      </body>
+    </html>
+    `;
+    return htmlTemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var counter = 0;
+app.get('/counter', function (req, res) {
+   counter = counter + 1;
+   res.send(counter.toString());
+});
+
+var names = [];
+app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
+  // Get the name from the request
+  var name = req.query.name;
+  
+  names.push(name);
+  // JSON: Javascript Object Notation
+  res.send(JSON.stringify(names));
+});
+
+app.get('/:articleName', function (req, res) {
+  // articleName == article-one
+  // articles[articleName] == {} content object for article one
+  var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/page-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'page-one.html'));
-});
-
-
-app.get('/page-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'page-two.html'));
-});
-
-app.get('/page-three', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'page-three.html'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
